21f809c7785ed28497cb55d1a819d2241995a525
brylenelavelle Patrick <brylenelavelle@gmail.com>
2023-03-27 15:33:37 +0000

    string chapter

--- string_case.rb
+++ string_case.rb
@@ -5,5 +5,7 @@
 # using a combination of the upcase, downcase, and swapcase methods.
 
 greeting = "hello"
-people = " FRIENDS"
-other_people = " aNd fAmIlY"
+people = "FRIENDS"
+other_people = "aNd fAmIlY"
+
+p "#{greeting.upcase} #{people.downcase} #{other_people.swapcase}"
